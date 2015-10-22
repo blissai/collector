3275fc68d8484759715524436d21d0b586c58a78
Michael Sive <mikesive@gmail.com>
2015-10-23 08:55:37 +1000

    add chocolatey to path immediately

---
+++ bliss-windows.rb
@@ -0,0 +1,1 @@
+`powershell.exe setup.ps1`
--- installscripts/chocolatey.ps1
+++ installscripts/chocolatey.ps1
@@ -1,1 +1,2 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+$env:path = $env:path += ";C:\ProgramData\chocolatey\bin"
