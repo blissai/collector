546bab5b893324f2afd6b62ce1656299213bd622
Michael Sive <mikesive@gmail.com>
2015-10-23 09:41:04 +1000

    altering bliss-installer

--- bliss-installer.rb
+++ bliss-installer.rb
@@ -1,5 +1,5 @@
 if Gem.win_platform?
-  `Set-ExecutionPolicy RemoteSigned; powershell.exe installscripts/setup.ps1`
+  `@powershell Set-ExecutionPolicy RemoteSigned; powershell.exe installscripts/setup.ps1`
 else
-  
+
 end
