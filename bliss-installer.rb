6571b181ce657a90791808702f5707db168c076e
Michael Sive <mikesive@gmail.com>
2015-10-23 15:44:24 +1000

    check to see if rake is already installed

--- bliss-installer.rb
+++ bliss-installer.rb
@@ -3,8 +3,9 @@ puts 'Installing Bliss Collector... please wait'
 `git clone https://github.com/founderbliss/collector.git C:/tools/blisscollector`
 puts 'Installing dependencies... please wait'
 `@powershell Set-ExecutionPolicy RemoteSigned; powershell.exe C:/tools/blisscollector/installscripts/setup.ps1`
-puts `cd C:/tools/blisscollector; gem install bundler; gem install rake`
-puts `cd C:/tools/blisscollector; bundle install`
+puts `gem install bundler` unless Gem::Specification::find_all_by_name('bundler').any?
+puts `gem install rake` unless Gem::Specification::find_all_by_name('rake').any?
+puts `bundle install`
 puts "Adding shortcut to desktop."
 `@powershell powershell.exe C:/tools/blisscollector/installscripts/shortcut.ps1`
 puts 'Installation complete.'
