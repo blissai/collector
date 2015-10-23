b395a0b5bff6cca029dc2697a007e2352c345056
Michael Sive <mikesive@gmail.com>
2015-10-23 10:41:43 +1000

    added script to add shortcut to windows desktop

--- bliss-installer.rb
+++ bliss-installer.rb
@@ -4,4 +4,5 @@ puts 'Installing dependencies...'
 `bundle install`
 `@powershell Set-ExecutionPolicy RemoteSigned; powershell.exe installscripts/setup.ps1`
 `git clone https://github.com/founderbliss/collector.git C:/tools/blisscollector`
+`@powershell powershell.exe installscripts/shortcut.ps1`
 puts 'Installation complete.'
