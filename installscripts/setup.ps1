5a15c3593ffb7d3ed777d6c09b432514f4d82bdd
Michael Sive <mikesive@gmail.com>
2015-10-26 08:55:43 +1000

    fixing powershell script

--- installscripts/setup.ps1
+++ installscripts/setup.ps1
@@ -7,13 +7,13 @@ choco feature enable -n=allowGlobalConfirmation
 #Install ruby and add to path
 Write-Host "Installing Ruby..."
 choco install ruby
-$env:path = "$($env:Path);C:\tools\ruby21\bin"
+$env:path = $env:Path + ";C:\tools\ruby21\bin"
 Write-Host "Installing Ruby Devkit..."
 choco install ruby2.devkit
 $oldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
-$newPath = "$($oldPath);C:\tools\DevKit2\bin"
+$newPath = $oldPath + ";C:\tools\DevKit2\bin"
 Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value $newPath
-$env:Path = "$($env:Path);C:\tools\DevKit2\bin"
+#$env:Path = "$($env:Path);C:\tools\DevKit2\bin"
 Write-Host "Installing Perl..."
 choco install strawberryperl
 Write-Host "Installing nodeJS..."
@@ -21,10 +21,10 @@ choco install nodejs
 choco install vcredist2012
 Write-Host "Installing PHP..."
 choco install php
-$oldPath=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
-$newPath= "$($oldPath);C:\tools\php"
-Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value $newPath
-$env:Path = "$($env:Path);C:\tools\php"
+$oldPathTwo=(Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).Path
+$newPathTwo= $oldPathTwo + ";C:\tools\php"
+Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH –Value $newPathTwo
+#$env:Path = "$($env:Path);C:\tools\php"
 Write-Host "Installing Python..."
 choco install python
 Write-Host "Installing Bundler..."
