153e635382b96c04bb81639c315beb133f7b04c2
Michael Sive <mikesive@gmail.com>
2015-10-26 08:06:12 +1000

    add puts statement to shell outputs

--- bliss-installer.rb
+++ bliss-installer.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 puts 'Installing Bliss Collector... please wait'
-`git clone https://github.com/founderbliss/collector.git C:/tools/blisscollector`
+puts `git clone https://github.com/founderbliss/collector.git C:/tools/blisscollector`
 puts 'Installing dependencies... please wait'
-`@powershell Set-ExecutionPolicy RemoteSigned; powershell.exe C:/tools/blisscollector/installscripts/setup.ps1`
+puts `@powershell Set-ExecutionPolicy RemoteSigned; powershell.exe C:/tools/blisscollector/installscripts/setup.ps1`
 puts 'Installation complete.'
