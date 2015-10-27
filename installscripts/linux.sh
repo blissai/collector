df81121412c699ac61cf725c7a426c4ab900900b
Michael Sive <mikesive@gmail.com>
2015-10-28 09:12:08 +1000

    fixing linux script

--- installscripts/linux.sh
+++ installscripts/linux.sh
@@ -8,14 +8,19 @@ if [[ ! -n "$(command -v rvm)" ]]
   yum install gcc-c++ patch readline readline-devel zlib zlib-devel
   yum install libyaml-devel libffi-devel openssl-devel make
   yum install bzip2 autoconf automake libtool bison iconv-devel
+  echo "Downloading RVM..."
   curl -sSL https://rvm.io/mpapis.asc | gpg --import -
   curl -L get.rvm.io | bash -s stable
+  echo "Installing RVM..."
   source /etc/profile.d/rvm.sh
+  echo "Reloading RVM..."
   rvm reload
+  echo "Installing RVM dependencies..."
   rvm requirements run
 fi
-echo "Setting Ruby version to 2.x..."
+echo "Download Ruby 2.x..."
 rvm install 2.0.0
+echo "Setting Ruby version to 2.x..."
 rvm use 2.0.0 --default
 if [[ ! -n "$(command -v npm)" ]]
 then
