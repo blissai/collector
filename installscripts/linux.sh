0807a95dacfb46bb32eebda11443059fd8af7381
Michael Sive <mikesive@gmail.com>
2015-10-27 11:07:13 +1000

    fixing linux.sh

--- installscripts/linux.sh
+++ installscripts/linux.sh
@@ -2,9 +2,9 @@
 if [[ -n "$(command -v yum)" ]]
 then
   sudo rpm --import https://rpm.packager.io/key
-  echo "[collector]
-  name=Repository for founderbliss/collector application.
-  baseurl=https://rpm.packager.io/gh/founderbliss/collector/centos6/master
+  echo -e "[collector]\n
+  name=Repository for founderbliss/collector application.\n
+  baseurl=https://rpm.packager.io/gh/founderbliss/collector/centos6/master\n
   enabled=1" | sudo tee /etc/yum.repos.d/collector.repo
   sudo yum install collector
 elif [[ -n "$(command -v apt-get)" ]]
