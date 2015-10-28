50f6d5cace3ca213ebc665ca24271a7565288577
Michael Sive <mikesive@gmail.com>
2015-10-29 08:38:37 +1000

    added loop around linters to do

--- lib/lintertask.rb
+++ lib/lintertask.rb
@@ -17,68 +17,69 @@ class LinterTask
       repo = repos[name]
       organization = repo['full_name'].split('/').first
       repo_key = repo['repo_key']
-
-      repo_return = agent.get(
-      "#{host}/api/gitlog/linters_todo?repo_key=#{repo_key}",
-      auth_headers)
-      json_return = JSON.parse(repo_return.body)
-
-      linters = json_return['linters']
-      linters.each do |linter|
-        ext = linter['output_format']
-        cd_first = linter['cd_first']
-        quality_tool = linter['quality_tool']
-        quality_command = linter ['quality_command']
+      loop do
+        repo_return = agent.get(
+        "#{host}/api/gitlog/linters_todo?repo_key=#{repo_key}",
+        auth_headers)
+        json_return = JSON.parse(repo_return.body)
         metrics = json_return['metrics']
-        puts "Working on repo: #{repo['full_name']}"
         puts "No recent commits to analyze." if metrics.empty?
-        metrics.each do |metric|
-          Dir.mktmpdir do |dir_name|
-            commit = metric['commit']
-            checkout_commit(git_dir, commit)
+        break if metrics.empty?
+        linters = json_return['linters']
+        linters.each do |linter|
+          ext = linter['output_format']
+          cd_first = linter['cd_first']
+          quality_tool = linter['quality_tool']
+          quality_command = linter ['quality_command']
+          puts "Working on repo: #{repo['full_name']}"
+          metrics.each do |metric|
+            Dir.mktmpdir do |dir_name|
+              commit = metric['commit']
+              checkout_commit(git_dir, commit)
 
-            remove_open_source_files(git_dir)
+              remove_open_source_files(git_dir)
 
-            proj_filename = nil
+              proj_filename = nil
 
-            file_name = File.join(dir_name, "#{quality_tool}.#{ext}")
-            cmd = quality_command.gsub('git_dir', git_dir).gsub('file_name', file_name).gsub('proj_filename', proj_filename.to_s)
-            cmd = get_cmd("cd #{git_dir};#{cmd}") if cd_first
-            puts "\tRunning linter: #{quality_tool}"
-            begin
-              `#{cmd}`
-              key = "#{organization}_#{name}_#{commit}.#{ext}"
-              object_params = {
-                bucket: 'founderbliss-temp-storage',
-                key: key,
-                body: File.open(file_name, 'r').read,
-                requester_pays: true,
-                acl: 'bucket-owner-read'
-              }
-              $aws_client.put_object(object_params)
-              lint_payload = {
-                commit: commit,
-                repo_key: repo_key,
-                linter_id: linter['id'],
-                lint_file_location: key }
+              file_name = File.join(dir_name, "#{quality_tool}.#{ext}")
+              cmd = quality_command.gsub('git_dir', git_dir).gsub('file_name', file_name).gsub('proj_filename', proj_filename.to_s)
+              cmd = get_cmd("cd #{git_dir};#{cmd}") if cd_first
+              puts "\tRunning linter: #{quality_tool}"
+              begin
+                `#{cmd}`
+                key = "#{organization}_#{name}_#{commit}.#{ext}"
+                object_params = {
+                  bucket: 'founderbliss-temp-storage',
+                  key: key,
+                  body: File.open(file_name, 'r').read,
+                  requester_pays: true,
+                  acl: 'bucket-owner-read'
+                }
+                $aws_client.put_object(object_params)
+                lint_payload = {
+                  commit: commit,
+                  repo_key: repo_key,
+                  linter_id: linter['id'],
+                  lint_file_location: key }
 
-              lint_response = agent.post("#{host}/api/commit/lint", lint_payload, auth_headers)
-              lint_return = JSON.parse(lint_response.body)
-            rescue Exception => e
-              puts "Your AWS Access Key is invalid...".red
-              $logger.error("#{Time.now}: Your AWS Access Key is invalid...") if  e.is_a? Aws::S3::Errors::InvalidAccessKeyId
-              if e.is_a? Errno::ENOENT
-                puts "#{quality_tool} is not installed. Please refer to the docs at https://github.com/founderbliss/collector to ensure all dependencies are installed.".red
-                $logger.warn("#{Time.now}: Dependency Error: #{quality_tool} not installed...")
+                  lint_response = agent.post("#{host}/api/commit/lint", lint_payload, auth_headers)
+                  lint_return = JSON.parse(lint_response.body)
+                rescue Exception => e
+                  puts "Your AWS Access Key is invalid...".red
+                  $logger.error("#{Time.now}: Your AWS Access Key is invalid...") if  e.is_a? Aws::S3::Errors::InvalidAccessKeyId
+                  if e.is_a? Errno::ENOENT
+                    puts "#{quality_tool} is not installed. Please refer to the docs at https://github.com/founderbliss/collector to ensure all dependencies are installed.".red
+                    $logger.warn("#{Time.now}: Dependency Error: #{quality_tool} not installed...")
+                  end
+                end
               end
             end
           end
+          # Go back to master at the end
+          checkout_commit(git_dir, 'master')
         end
-        # Go back to master at the end
-        checkout_commit(git_dir, 'master')
+        puts dir_names.join
       end
-      puts dir_names.join
+      puts "Linter finished.".green
     end
-    puts "Linter finished.".green
   end
-end
