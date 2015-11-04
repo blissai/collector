6488983c43694ce6e7779bf9cac2f5c68d7865c0
Michael Sive <mikesive@gmail.com>
2015-11-04 15:17:35 +1000

    remove binding.pry

--- lib/collectortask.rb
+++ lib/collectortask.rb
@@ -24,7 +24,6 @@ class CollectorTask
     # command = "cd #{dir_name};git log --all --pretty=format:'#{log_fmt}' #{since_param}"
     # cmd = get_cmd(command)
     cmd = "cd #{dir_name} && git log --all --pretty=format:#{log_fmt} #{since_param}"
-    binding.pry
     `#{cmd}`
   end
 
