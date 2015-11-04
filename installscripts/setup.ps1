79929cb65d376764ec5f5591d718b064907358bc
Michael Sive <mikesive@gmail.com>
2015-11-04 11:12:12 +1000

    setup.ps1

--- installscripts/setup.ps1
+++ installscripts/setup.ps1
@@ -2,12 +2,13 @@ Set-ExecutionPolicy RemoteSigned
 Write-Host "Installing Chocolatey Package Manager...";
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'));
 $env:path = $env:path += ";C:\ProgramData\chocolatey\bin";
-
-#Install Java/JRuby
-choco install javaruntime
-choco install jruby
 #Allow global confirmation on install
 choco feature enable -n=allowGlobalConfirmation;
+#Install Java/JRuby
+Write-Host "Installing Java...";
+choco install javaruntime
+Write-Host "Installing JRuby...";
+choco install jruby
 Write-Host "Installing Perl...";
 choco install strawberryperl;
 Write-Host "Installing nodeJS...";
