ad4186c92a6fcb348f68d528f430bff574d818a1
Michael Sive <mikesive@gmail.com>
2015-11-05 09:24:57 +1000

    remove loop around lint task

--- lib/lintertask.rb
+++ lib/lintertask.rb
@@ -21,57 +21,53 @@ class LinterTask
     @logger.info("Running Linter on #{name}...")
     organization = repo['full_name'].split('/').first
     repo_key = repo['repo_key']
-    loop do
-      json_return = http_get(agent, "#{host}/api/gitlog/linters_todo?repo_key=#{repo_key}", auth_headers)
-      metrics = json_return['metrics']
-      linters = json_return['linters']
-      break if metrics.empty?
-      break if linters.empty?
-      metrics.each do |metric|
-        commit = metric['commit']
-        checkout_commit(git_dir, commit)
-        remove_open_source_files(git_dir)
-          linters.each do |linter|
-            ext = linter['output_format']
-            cd_first = linter['cd_first']
-            quality_tool = linter['quality_tool']
-            quality_command = linter['quality_command']
-            proj_filename = nil
-            file_name = File.join(git_dir, "#{quality_tool}.#{ext}")
-            cmd = quality_command.gsub('git_dir', git_dir).gsub('file_name', file_name).gsub('proj_filename', proj_filename.to_s)
-            # cmd = get_cmd("cd #{git_dir};#{cmd}") if cd_first
-            cmd = "cd #{git_dir} && #{cmd}" if cd_first
-            puts "\tRunning linter: #{quality_tool}... This may take a while... (#{total_lints_done + 1} / #{total_lints_count})".blue
-            @logger.info("Running #{quality_tool} on #{commit}...")
-            begin
-              lint_output = `#{cmd}`
-              if !quality_tool.include? 'cpd'
-                lint_output = File.open(file_name, 'r').read
-              end
-              puts "\tUploading lint results to AWS...".blue
-              key = "#{organization}_#{name}_#{commit}_#{quality_tool}.#{ext}"
-              object_params = {
-                bucket: 'founderbliss-temp-storage',
-                key: key,
-                body: lint_output,
-                requester_pays: true,
-                acl: 'bucket-owner-read'
-              }
-              $aws_client.put_object(object_params)
-              lint_payload = { commit: commit, repo_key: repo_key, linter_id: linter['id'], lint_file_location: key }
-              lint_response = http_post(agent, "#{host}/api/commit/lint", lint_payload, auth_headers)
-              File.delete(file_name)
-            rescue Aws::S3::Errors::InvalidAccessKeyId
-              puts "Your AWS Access Key is invalid...".red
-              @logger.error("Your AWS Access Key is invalid...")
-            rescue Errno::ENOENT
-              puts "#{quality_tool} is not installed. Please refer to the docs at https://github.com/founderbliss/collector to ensure all dependencies are installed.".red
-              @logger.info("Dependency Error: #{quality_tool} not installed...")
-            end
-            total_lints_done += 1
-            percent_done = ((total_lints_done.to_f / total_lints_count.to_f) * 100).round(2) rescue 100
-            puts "\n\n Finished #{total_lints_done} of #{total_lints_count} lint tasks (#{percent_done}%) for #{name.upcase} \n\n".green
+    json_return = http_get(agent, "#{host}/api/gitlog/linters_todo?repo_key=#{repo_key}", auth_headers)
+    metrics = json_return['metrics']
+    linters = json_return['linters']
+    metrics.each do |metric|
+      commit = metric['commit']
+      checkout_commit(git_dir, commit)
+      remove_open_source_files(git_dir)
+      linters.each do |linter|
+        ext = linter['output_format']
+        cd_first = linter['cd_first']
+        quality_tool = linter['quality_tool']
+        quality_command = linter['quality_command']
+        proj_filename = nil
+        file_name = File.join(git_dir, "#{quality_tool}.#{ext}")
+        cmd = quality_command.gsub('git_dir', git_dir).gsub('file_name', file_name).gsub('proj_filename', proj_filename.to_s)
+        # cmd = get_cmd("cd #{git_dir};#{cmd}") if cd_first
+        cmd = "cd #{git_dir} && #{cmd}" if cd_first
+        puts "\tRunning linter: #{quality_tool}... This may take a while... (#{total_lints_done + 1} / #{total_lints_count})".blue
+        @logger.info("Running #{quality_tool} on #{commit}...")
+        begin
+          lint_output = `#{cmd}`
+          if !quality_tool.include? 'cpd'
+            lint_output = File.open(file_name, 'r').read
           end
+          puts "\tUploading lint results to AWS...".blue
+          key = "#{organization}_#{name}_#{commit}_#{quality_tool}.#{ext}"
+          object_params = {
+            bucket: 'founderbliss-temp-storage',
+            key: key,
+            body: lint_output,
+            requester_pays: true,
+            acl: 'bucket-owner-read'
+          }
+          $aws_client.put_object(object_params)
+          lint_payload = { commit: commit, repo_key: repo_key, linter_id: linter['id'], lint_file_location: key }
+          lint_response = http_post(agent, "#{host}/api/commit/lint", lint_payload, auth_headers)
+          File.delete(file_name)
+        rescue Aws::S3::Errors::InvalidAccessKeyId
+          puts "Your AWS Access Key is invalid...".red
+          @logger.error("Your AWS Access Key is invalid...")
+        rescue Errno::ENOENT
+          puts "#{quality_tool} is not installed. Please refer to the docs at https://github.com/founderbliss/collector to ensure all dependencies are installed.".red
+          @logger.info("Dependency Error: #{quality_tool} not installed...")
+        end
+        total_lints_done += 1
+        percent_done = ((total_lints_done.to_f / total_lints_count.to_f) * 100).round(2) rescue 100
+        puts "\n\n Finished #{total_lints_done} of #{total_lints_count} lint tasks (#{percent_done}%) for #{name.upcase} \n\n".green
       end
       # Go back to master at the end
       checkout_commit(git_dir, 'master')
