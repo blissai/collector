dc4588a938eca842bab3f1b0162676b11ee5a8a8
Michael Sive <mikesive@gmail.com>
2015-12-14 13:13:35 +1000

    remove symlink, change jruby to be consistent, check for brew/rvm

--- installscripts/osx.sh
+++ installscripts/osx.sh
@@ -1,23 +1,24 @@
 ##!/bin/bash
 echo "Downloading RVM..."
-curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-curl -L get.rvm.io | bash -s stable
-source /etc/profile.d/rvm.sh
-echo "Reloading RVM..."
-rvm reload
-echo "Installing RVM dependencies..."
-rvm requirements run
-echo "Downloading JRuby 9.0.3.0..."
-rvm install jruby-9.0.0.0
-echo "Setting Ruby version to JRuby-9.0.3.0..."
-rvm use jruby-9.0.3.0 --default
-rvm reload
-if [[ ! -n "$(command -v npm)" ]]
-  then
-  echo "Node not installed. Installing..."
-  brew install nodejs
+if [[ ! -n "$(command -v rvm)" ]]
+then
+  echo "RVM not installed. Please make sure RVM and Homebrew are installed before proceeding."
+elif [[ ! -n "$(command -v brew)" ]]
+then
+  echo "Homebrew not installed. Please make sure RVM and Homebrew are installed before proceeding."
+else
+  echo "Downloading JRuby 9.0.3.0..."
+  rvm install jruby-9.0.3.0
+  echo "Setting Ruby version to JRuby-9.0.3.0..."
+  rvm use jruby-9.0.3.0 --default
+  rvm reload
+  if [[ ! -n "$(command -v npm)" ]]
+    then
+    echo "Node not installed. Installing..."
+    brew install nodejs
+  fi
+  git clone https://github.com/founderbliss/collector.git ~/collector
+  sudo chmod +x ~/collector/collector.sh
+# sudo ln -s  ~/collector/collector.sh /usr/bin/collector
+  echo "Installation complete. Please reboot your system."
 fi
-git clone https://github.com/founderbliss/collector.git ~/collector
-sudo chmod +x ~/collector/collector.sh
-sudo ln -s  ~/collector/collector.sh /usr/bin/collector
-echo "Installation complete. Please reboot your system."
