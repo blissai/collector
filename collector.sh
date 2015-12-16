d6438fdf54b9f5f725868071e7385ca360124d45
Michael Sive <mikesive@gmail.com>
2015-12-16 11:02:27 +1000

    remove quiet

--- collector.sh
+++ collector.sh
@@ -18,7 +18,8 @@ else
 fi
 ABSOLUTE_PATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
 cd $ABSOLUTE_PATH
-
+echo "Installing bundler"
 gem install bundler
-bundle install --quiet
+echo "Running bundle install run"
+bundle install
 ruby $ABSOLUTE_PATH/blisscollector.rb
