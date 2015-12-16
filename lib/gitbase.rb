6836cabf7b62ffa22042e72f8041d9b943db79e8
Michael Sive <mikesive@gmail.com>
2015-12-16 14:53:35 +1000

    add statistic sense_project_type

--- lib/gitbase.rb
+++ lib/gitbase.rb
@@ -12,23 +12,22 @@ module Gitbase
     co_cmd = "cd #{git_dir} && git checkout #{commit}"
     stdin, stdout, stderr = Open3.popen3(co_cmd)
     @ref = nil
-    while (err = stderr.gets) do
+    while (err = stderr.gets)
       puts err unless err.include? "Already on 'master'"
       @ref = err
-      if err =~ /Your local changes to the following files would be overwritten by checkout/
-        `#{remove_command} #{git_dir}/*`
-        # cmd = get_cmd("cd #{git_dir};git checkout #{co_cmd}")
-        cmd = "cd #{git_dir} && git checkout #{commit}"
-        `#{cmd}`
-        # cmd = get_cmd("cd #{git_dir};git reset --hard HEAD")
-        cmd = "cd #{git_dir} && git reset --hard HEAD"
-        `#{cmd}`
-        # cmd = get_cmd("cd #{git_dir};git clean -fdx")
-        cmd = "cd #{git_dir} && git clean -fdx"
-        `#{cmd}`
-        @ref = `#{co_cmd}`
-        break
-      end
+      next unless err =~ /Your local changes to the following files would be overwritten by checkout/
+      `#{remove_command} #{git_dir}/*`
+      # cmd = get_cmd("cd #{git_dir};git checkout #{co_cmd}")
+      cmd = "cd #{git_dir} && git checkout #{commit}"
+      `#{cmd}`
+      # cmd = get_cmd("cd #{git_dir};git reset --hard HEAD")
+      cmd = "cd #{git_dir} && git reset --hard HEAD"
+      `#{cmd}`
+      # cmd = get_cmd("cd #{git_dir};git clean -fdx")
+      cmd = "cd #{git_dir} && git clean -fdx"
+      `#{cmd}`
+      @ref = `#{co_cmd}`
+      break
     end
   end
 
@@ -37,11 +36,11 @@ module Gitbase
     puts "\tRemoving open source files...".blue
     open_source_lines = nil
     if Gem.win_platform?
-      egrep_cmd = "C:/Program Files (x86)/GnuWin32/bin/egrep.exe"
+      egrep_cmd = 'C:/Program Files (x86)/GnuWin32/bin/egrep.exe'
       if File.exist?(egrep_cmd)
         open_source_lines = `"#{egrep_cmd}" -i "free software|Hamano|jQuery|BSD|GPL|GNU|MIT|Apache" #{git_dir}/* -R`.split("\n").keep_if do |line|
           begin
-            line.encode('UTF-8', :invalid => :replace) =~ /License|Copyright/i
+            line.encode('UTF-8', invalid: :replace) =~ /License|Copyright/i
           rescue Encoding::UndefinedConversionError => e
             false
           end
@@ -49,7 +48,7 @@ module Gitbase
       else
         open_source_lines = `findstr /R /S "Hamano jQuery BSD GPL GNU MIT Apache" #{git_dir}/*`.split("\n").keep_if do |line|
           begin
-            line.encode('UTF-8', :invalid => :replace) =~ /License|Copyright/i
+            line.encode('UTF-8', invalid: :replace) =~ /License|Copyright/i
           rescue Encoding::UndefinedConversionError => e
             false
           end
@@ -58,22 +57,22 @@ module Gitbase
     else
       open_source_lines = `egrep -i "free software|Hamano|jQuery|BSD|GPL|GNU|MIT|Apache" #{git_dir}/* -R`.split("\n").keep_if do |line|
         begin
-          line.encode('UTF-8', :invalid => :replace) =~ /License|Copyright/i
+          line.encode('UTF-8', invalid: :replace) =~ /License|Copyright/i
         rescue Encoding::UndefinedConversionError => e
           false
         end
       end
     end
     todo = []
-    temp_start = Gem.win_platform? ? "c:/temp/codecop" : "/tmp/codecop"
+    temp_start = Gem.win_platform? ? 'c:/temp/codecop' : '/tmp/codecop'
     open_source_lines.each do |line|
-      line = line.gsub("\\", "/")
+      line = line.tr('\\', '/')
       if match = /^#{temp_start}([^:]+?)\/[^\/:\s]*license|licence|readme|(.txt|.md):/i.match(line)
-        #puts "license file found: #{line}"
+        # puts "license file found: #{line}"
         file_name = "#{temp_start}#{match[1]}"
         todo << ["#{remove_command} #{file_name}/*", file_name] if match[1]
       elsif match = /^#{temp_start}([^:]+?)\/[^\/]*manifest.xml:/i.match(line)
-        #puts "manifest file found: #{line}"
+        # puts "manifest file found: #{line}"
         file_name = "#{temp_start}#{match[1]}"
         todo << ["#{remove_command} #{file_name}/*", file_name] if match[1]
       elsif match = /^#{temp_start}([^:]+?):/i.match(line)
@@ -83,13 +82,13 @@ module Gitbase
       if true
       end
     end
-    if File.exist?(File.join(git_dir,'NuGet.config')) && Dir.exist?(File.join(git_dir,'packages'))
+    if File.exist?(File.join(git_dir, 'NuGet.config')) && Dir.exist?(File.join(git_dir, 'packages'))
       file_name = File.join(git_dir, 'packages')
       todo << ["#{remove_command} #{file_name}", file_name]
     end
     todo.uniq!
     todo.each do |cmd, file_name|
-      #puts cmd
+      # puts cmd
       puts "Removing #{file_name}"
       if File.exist?(file_name)
         # `#{get_cmd(cmd)}`
@@ -98,14 +97,14 @@ module Gitbase
     end
   end
 
-  def find_copyright(git_dir, is_demo=false)
+  def find_copyright(git_dir, is_demo = false)
     puts "Finding copyrights: #{git_dir}".blue
     owners = []
     egrep_cmd = Gem.win_platform? ? "\"C:/Program Files (x86)/GnuWin32/bin/egrep.exe\"" : 'egrep'
     copyright_lines = `#{egrep_cmd} -i "copyright|\(c\)|\&copy\;" #{git_dir}/* -R`
-    copyright_lines.encode('UTF-8', :invalid => :replace).split("\n").each do |line|
+    copyright_lines.encode('UTF-8', invalid: :replace).split("\n").each do |line|
       owner, file = Copyright.find_owner(line)
-      next if is_demo and (file =~ /fixture/)
+      next if is_demo && (file =~ /fixture/)
       owners << owner
     end
     owners = owners.compact.uniq
@@ -114,7 +113,7 @@ module Gitbase
   end
 
   def cloc_options
-    "--yaml --quiet --skip-uniqueness --progress-rate 0"
+    '--yaml --quiet --skip-uniqueness --progress-rate 0'
   end
 
   def cloc_command
@@ -123,14 +122,14 @@ module Gitbase
     # else
     #   "bin/cloc"
     # end
-    "bin/cloc"
+    'bin/cloc'
   end
 
   def remove_command
     if Gem.win_platform?
-      "rm -r -Force"
+      'rm -r -Force'
     else
-      "rm -rf"
+      'rm -rf'
     end
   end
 
@@ -139,50 +138,71 @@ module Gitbase
     languages = []
     if Dir.entries(git_dir).find { |e| /\.sln$/ =~ e }
       # language = ".NET"
-      languages.push(".NET")
-    elsif !Dir.glob(File.join(git_dir, "**/*.rb")).empty?
-      languages.push("ruby")
-      if File.exist?(File.join(git_dir,"config","boot.rb"))
+      languages.push('.NET')
+    elsif !Dir.glob(File.join(git_dir, '**/*.rb')).empty?
+      languages.push('ruby')
+      if File.exist?(File.join(git_dir, 'config', 'boot.rb'))
         # language = 'rails'
-        languages.push("rails")
+        languages.push('rails')
       end
-    elsif File.exist?(File.join(git_dir,"Podfile")) || !Dir.glob(File.join(git_dir, "**/*.xcodeproj")).empty?
+    elsif File.exist?(File.join(git_dir, 'Podfile')) || !Dir.glob(File.join(git_dir, '**/*.xcodeproj')).empty?
       # language = 'ios'
-      languages.push("ios")
-      languages.push("Objective-C")
-    elsif File.exist?(File.join(git_dir, "Godeps"))
+      languages.push('ios')
+      languages.push('Objective-C')
+    elsif File.exist?(File.join(git_dir, 'Godeps'))
       # language = 'go'
-      languages.push("go")
-    elsif File.directory?(File.join(git_dir, "wp-content"))
+      languages.push('go')
+    elsif File.directory?(File.join(git_dir, 'wp-content'))
       # language = 'wordpress'
-      languages.push("wordpress")
-    elsif !Dir.glob(File.join(git_dir, "**/*.php")).empty?
-      if File.exist?(File.join(git_dir, "server.php")) && file_contains("#{git_dir}/server.php", /package[ ]+Laravel/)
-        languages.push("Laravel")
-      elsif File.exist?(File.join(git_dir, "index.php")) && file_contains("#{git_dir}/index.php", "package Elgg")
-        languages.push("elgg")
+      languages.push('wordpress')
+    elsif !Dir.glob(File.join(git_dir, '**/*.php')).empty?
+      if File.exist?(File.join(git_dir, 'server.php')) && file_contains("#{git_dir}/server.php", /package[ ]+Laravel/)
+        languages.push('Laravel')
+      elsif File.exist?(File.join(git_dir, 'index.php')) && file_contains("#{git_dir}/index.php", 'package Elgg')
+        languages.push('elgg')
       else
-        languages.push("php")
+        languages.push('php')
       end
-    elsif !Dir.glob(File.join(git_dir, "*.py")).empty?
-      if File.exists?(File.join(git_dir, "manage.py"))
-        if file_contains("#{git_dir}/manage.py", "django")
+    elsif !Dir.glob(File.join(git_dir, '*.py')).empty?
+      if File.exist?(File.join(git_dir, 'manage.py'))
+        if file_contains("#{git_dir}/manage.py", 'django')
           # language = 'django'
-          languages.push("django")
+          languages.push('django')
         end
       end
       # language = 'Python'
-      languages.push("Python")
-    elsif File.exist?(File.join(git_dir, "package.json"))
+      languages.push('Python')
+    elsif File.exist?(File.join(git_dir, 'package.json'))
       # language = 'nodejs'
-      languages.push("nodejs")
-    elsif !Dir.glob(File.join(git_dir, "**/*.java")).empty? || File.exist?(File.join(git_dir, "build.gradle"))
+      languages.push('nodejs')
+    elsif !Dir.glob(File.join(git_dir, '**/*.java')).empty? || File.exist?(File.join(git_dir, 'build.gradle'))
       # language = 'Java'
-      languages.push("Java")
-    elsif !Dir.glob(File.join(git_dir, "**/*.pm")).empty? || !Dir.glob(File.join(git_dir, "**/*.pl")).empty?
-      languages.push("Perl")
+      languages.push('Java')
+    elsif !Dir.glob(File.join(git_dir, '**/*.pm')).empty? || !Dir.glob(File.join(git_dir, '**/*.pl')).empty?
+      languages.push('Perl')
+    end
+    if languages.empty?
+      cloc_lang = sense_project_type_cloc(git_dir)
+      languages.push(cloc_lang) unless cloc_lang.nil
     end
     languages
+  end
+
+  def sense_project_type_cloc(git_dir)
+    # Go for most common language
+    cloc = `perl #{cloc_command} #{git_dir} #{cloc_options}`
+    cloc_hash = YAML.load(cloc)
+    max = 0
+    language = nil
+    if cloc_hash.present?
+      cloc_hash.each_pair do |lang, values|
+        if max < values['code'].to_i
+          language = lang
+          max = values['code'].to_i
+        end
+      end
+    end
+    language
   end
 
   def file_contains(file_path, search_string)
